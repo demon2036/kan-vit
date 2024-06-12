29afa021efd24ba295b8ad6c33bd3e6f2fc785c5
jtitor <1165137798@qq.com>
2024-06-12 23:31:03 +0800

    first commit

--- config/deit/mae/mae-deit3-b16-pt-192-in1k-400ep-ft-224-in1k-100ep.sh
+++ config/deit/mae/mae-deit3-b16-pt-192-in1k-400ep-ft-224-in1k-100ep.sh
@@ -1,12 +1,12 @@
-export train_batch_size=512 warmup_epoch=5 epoch=100
+export train_batch_size=1024 warmup_epoch=5 epoch=100
 ## $GCS_MODEL_DIR/mae/mae-deit-b16-224-in1k-800ep-last.msgpack \
 python3 src/main.py \
     --output-dir $GCS_MODEL_DIR/mae \
     --pretrained-ckpt $GCS_MODEL_DIR/mae/mae-deit-b16-224-in1k-800ep-last.msgpack \
     --train-dataset-shards "$GCS_DATASET_DIR/imagenet-1k-wds/imagenet1k-train-{0000..1023}.tar" \
     --valid-dataset-shards "$GCS_DATASET_DIR/imagenet-1k-wds/imagenet1k-validation-{00..63}.tar" \
-    --train-batch-size 512 \
-    --valid-batch-size 512 \
+    --train-batch-size $train_batch_size \
+    --valid-batch-size 1024 \
     --train-loader-workers 40 \
     --valid-loader-workers 10 \
     --random-crop rrc \
@@ -35,18 +35,18 @@ python3 src/main.py \
     --dropout-seed 0 \
     --shuffle-seed 0 \
     --optimizer adamw \
-    --learning-rate 1e-4 \
-    --weight-decay 0.1 \
+    --learning-rate 4e-3 \
+    --weight-decay 0.05 \
     --adam-b1 0.9 \
     --adam-b2 0.999 \
     --adam-eps 1e-8 \
     --lr-decay 1.0 \
     --clip-grad 0.0 \
     --grad-accum 1 \
-    --warmup-steps $((1281167 * 5 / 512)) \
-    --training-steps $((1281167 * $epoch / 512)) \
+    --warmup-steps $((1281167 * 5 / $train_batch_size)) \
+    --training-steps $((1281167 * $epoch / $train_batch_size)) \
     --log-interval 100 \
-    --eval-interval $((1281167 * 1 / 512)) \
+    --eval-interval $((1281167 * 1 / $train_batch_size)) \
     --project deit3-jax \
     --name $(basename $0 .sh) \
     --ipaddr $(curl -s ifconfig.me) \
