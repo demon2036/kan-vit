ac0b4c542b0174fceb6451395ea624531a014124
jtitor <1165137798@qq.com>
2024-06-13 17:37:17 +0800

    first commit

---
+++ config/deit/mae/mae-kan-deit-b16-224-in1k-800ep.sh
@@ -0,0 +1,50 @@
+export train_batch_size=4096 warmup_epoch=40 epoch=800
+
+python3 src/main_pretrain_mae.py \
+    --output-dir $GCS_MODEL_DIR/mae \
+    --train-dataset-shards "$GCS_DATASET_DIR/imagenet-1k-wds/imagenet1k-train-{0000..1023}.tar" \
+    --train-batch-size $train_batch_size \
+    --train-loader-workers 40 \
+    --random-crop rrc \
+    --color-jitter 0.0 \
+    --random-erasing 0.0 \
+    --augment-repeats 1 \
+    --test-crop-ratio 1.0 \
+    --mixup 0.8 \
+    --cutmix 1.0 \
+    --criterion bce \
+    --label-smoothing 0.0 \
+    --layers 12 \
+    --dim 768 \
+    --heads 12 \
+    --labels 1000 \
+    --layerscale \
+    --patch-size 16 \
+    --image-size 224 \
+    --posemb learnable \
+    --pooling gap \
+    --dropout 0.0 \
+    --droppath 0.1 \
+    --init-seed 0 \
+    --mixup-seed 0 \
+    --dropout-seed 0 \
+    --shuffle-seed 0 \
+    --optimizer lamb \
+    --learning-rate 2.4e-3 \
+    --weight-decay 0.05 \
+    --adam-b1 0.9 \
+    --adam-b2 0.999 \
+    --adam-eps 1e-8 \
+    --lr-decay 1.0 \
+    --clip-grad 1.0 \
+    --grad-accum 1 \
+    --warmup-steps $((1281167 * $warmup_epoch / $train_batch_size)) \
+    --training-steps $((1281167 * $epoch / $train_batch_size)) \
+    --log-interval 100 \
+    --eval-interval $((1281167 * 5 / $train_batch_size)) \
+    --project deit3-jax-mae \
+    --name $(basename $0 .sh) \
+    --ipaddr $(curl -s ifconfig.me) \
+    --hostname $(hostname) \
+    --use-kan \
+    --polynomial-degree 8 \
