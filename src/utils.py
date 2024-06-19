161b0196c5306465e663d9aad2bfbc355bf6c368
jtitor <1165137798@qq.com>
2024-06-19 10:35:15 +0800

    first commit

--- src/utils.py
+++ src/utils.py
@@ -155,7 +155,7 @@ def load_pretrained_params(args: argparse.Namespace, params: ArrayTree) -> Array
 
     with wds.gopen(args.pretrained_ckpt,bufsize=8192*2**5) as fp:
         new_params = flax.serialization.msgpack_restore(fp.read())
-
+    print(new_params["model"]["embed"]["wpe"].shape,params["model"]["embed"]["wpe"].shape)
     # The positional embeddings will be resized when there is a difference in image
     # resolutions between pretraining and finetuning stage.
     if (
