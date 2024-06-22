362cdaee4c2764d5fdd1bde427eda4d90c3dfda3
jtitor <1165137798@qq.com>
2024-06-22 19:05:05 +0800

    first commit

--- config/deit/mae/eval_mae.sh
+++ config/deit/mae/eval_mae.sh
@@ -3,22 +3,19 @@ export train_batch_size=1024 warmup_epoch=5 epoch=100
 python3 src/main_pretrain_mae_eval.py \
     --output-dir $GCS_MODEL_DIR/mae \
     --pretrained-ckpt $GCS_MODEL_DIR/mae/mae_base_pretrain.msgpack \
+    -output-dir $GCS_MODEL_DIR/mae \
     --train-dataset-shards "$GCS_DATASET_DIR/imagenet-1k-wds/imagenet1k-train-{0000..1023}.tar" \
-    --valid-dataset-shards "$GCS_DATASET_DIR/imagenet-1k-wds/imagenet1k-validation-{00..63}.tar" \
     --train-batch-size $train_batch_size \
-    --valid-batch-size 1024 \
-    --train-loader-workers 40 \
-    --valid-loader-workers 10 \
+    --train-loader-workers 80 \
     --random-crop rrc \
     --color-jitter 0.0 \
-    --auto-augment rand-m9-mstd0.5-inc1 \
-    --random-erasing 0.25 \
+    --random-erasing 0.0 \
     --augment-repeats 1 \
-    --test-crop-ratio 0.875 \
+    --test-crop-ratio 1.0 \
     --mixup 0.8 \
     --cutmix 1.0 \
-    --criterion ce \
-    --label-smoothing 0.1 \
+    --criterion bce \
+    --label-smoothing 0.0 \
     --layers 12 \
     --dim 768 \
     --heads 12 \
@@ -28,24 +25,24 @@ python3 src/main_pretrain_mae_eval.py \
     --posemb learnable \
     --pooling cls \
     --dropout 0.0 \
-    --droppath 0.1 \
+    --droppath 0.0 \
     --init-seed 0 \
     --mixup-seed 0 \
     --dropout-seed 0 \
-    --shuffle-seed 3407 \
+    --shuffle-seed 0 \
     --optimizer adamw \
-    --learning-rate 2e-3 \
+    --learning-rate 2.4e-3 \
     --weight-decay 0.05 \
     --adam-b1 0.9 \
-    --adam-b2 0.999 \
+    --adam-b2 0.95 \
     --adam-eps 1e-8 \
-    --lr-decay 0.65 \
+    --lr-decay 1.0 \
     --clip-grad 0.0 \
     --grad-accum 1 \
-    --warmup-steps $((1281167 * 5 / $train_batch_size)) \
+    --warmup-steps $((1281167 * $warmup_epoch / $train_batch_size)) \
     --training-steps $((1281167 * $epoch / $train_batch_size)) \
     --log-interval 100 \
-    --eval-interval $((1281167 * 1 / $train_batch_size)) \
+    --eval-interval $((1281167 * 5 / $train_batch_size)) \
     --project deit3-jax-mae \
     --name $(basename $0 .sh) \
     --ipaddr $(curl -s ifconfig.me) \
