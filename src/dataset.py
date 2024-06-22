ffdb84548e8d2212ca489d45761c38fb70f99617
jtitor <1165137798@qq.com>
2024-06-22 21:30:34 +0800

    first commit

--- src/dataset.py
+++ src/dataset.py
@@ -83,6 +83,9 @@ def create_transforms(args: argparse.Namespace) -> tuple[nn.Module, nn.Module]:
         T.PILToTensor(),
     ]
 
+
+    print(args.auto_augment)
+
     # train_transforms = [
     #     T.RandomResizedCrop(args.image_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
     #     T.RandomHorizontalFlip(),
